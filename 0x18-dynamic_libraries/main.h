0f4389ce684f30e2277dd6ee1e485713701363fa
Julius <fadafigo100@gmail.com>
2023-03-20 17:39:33 -0700

    advanced task

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+int add(int, int);
+int sub(int, int);
+int div(int, int);
+int mul(int, int);
+int mod(int, int);
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -20,4 +26,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
+
+#endif /* MAIN_H */
